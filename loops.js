8680bea8d6022138b4b4d27325f854bcbd8a130d
Jody Neckles <jody@technative.tv>
2018-10-19 21:32:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array){
   var array = [] // create an empty array
   for (let i = 0; i < 25; i++) { // set initial value to 1. execute the function block and increment value by 1 until it reaches 25.
-    if (i > 1) {
-      array.push("I am " + [i] + " strange loops.") // Else push this string to the array.
+    if (i === 1) {
+      array.push("I am 1 strange loop.") // If the value is equal to 1 then push the string to array.
     }
     else {
-        array.push("I am 1 strange loop.") // If the value is equal to 1 then push the string to array.
+        array.push("I am " + [i] + " strange loops.") // Else push this string to the array.
       }
   }
   return array
