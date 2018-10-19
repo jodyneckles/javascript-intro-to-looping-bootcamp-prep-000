f7d78fe91090e373bd760011d6a454f06d32ec6d
Jody Neckles <jody@technative.tv>
2018-10-19 21:29:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@ function forLoop(array){
       array.push("I am 1 strange loop.") // If the value is equal to 1 then push the string to array.
     }
     else {
-        array.push("I am ${i} strange loops.") // Else push this string to the array.
+        array.push("I am" + [i] + "strange loops.") // Else push this string to the array.
       }
   }
   return array
