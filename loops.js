18932c837ff5bd81f4834f02eab566bc5a92cbb4
Jody Neckles <jody@technative.tv>
2018-10-19 20:54:35 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function makeArray() { // This function is to make an array with 25 values and strings
+  var array = [] // create an empty array
+  for (let i = 1; i < 25; i++) { // set initial value to 1. execute the function block and increment value by 1 until it reaches 25.
+    if (i === 1) {
+      array.push("I am 1 strange loop.") // If the value is equal to 1 then push the string to array.
+    } else {
+        array.push("I am " + i + " strange loops."); // Else push this string to the array.
+      }
+  }
+  console.log(array) // 
+}
